56cd8b31bb0d7524387c62da6c9f0135fbbf1d85
Tareq Abdou <138627790+Tar3qXD@users.noreply.github.com>
2026-06-30 16:04:03 -0400

    Add Arduino photoresistor test code

---
+++ photoresistor_test.ino
@@ -0,0 +1,19 @@
+const int sensorPin = A0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensorValue = analogRead(sensorPin);
+  float voltage = sensorValue * (5.0 / 1023.0);
+
+  Serial.print("Raw Value: ");
+  Serial.print(sensorValue);
+
+  Serial.print(" | Voltage: ");
+  Serial.print(voltage);
+  Serial.println(" V");
+
+  delay(500);
+}
